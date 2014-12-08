072dc98c00f41509415f32413a42890ed2a98382
Joe Elsey <joe.elsey@gmail.com>
2014-12-08 10:59:46 -0800

    jwt begin

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
+var passport = require('passport');
 
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/canvas_dev');
 
+app.set('jwtSecret', process.env.JWT_SECRET || 'changethisordie');
+
 app.use(bodyparser.json());
+app.use(passport.initialize());
 
 require('./routes/index.js')(app);
+var jwtauth = require('./lib/jwt_auth')(app.get('jwtSecret'));
+
+var canvasRouter = express.Router();
+canvasRouter.use(jwtauth);
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
