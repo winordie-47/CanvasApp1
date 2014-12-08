376c0d5cd6d2ee60045ae6a4fdb2f3dcb650bd97
Joe Elsey <joe.elsey@gmail.com>
2014-12-08 11:28:16 -0800

    user jwt

--- models/user_model.js
+++ models/user_model.js
@@ -1,13 +1,37 @@
 'use strict';
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+
+var expires = moment().add(7, 'days').valueOf();
+console.log(expires);
 
 var userSchema = mongoose.Schema({
-  username: email,
-  userclass: [],
-  teacher: false
-
+  basic:{
+    email: String,
+    password: String,
+    teacher: false,
+    userclass: []
+  }
 });
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.basic.password);
+};
+
+userSchema.methods.generateToken = function(secret) {
+  var self = this;
+  var token = jwt.encode({
+    iss: self._id,
+    exp: expires
+  }, secret);
+  return token;
+};
 
 module.exports = mongoose.model('User', userSchema);
