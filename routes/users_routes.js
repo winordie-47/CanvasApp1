3606c63d6ecc3a15da49d9658d79a5a0e7c94817
Joe Elsey <joe.elsey@gmail.com>
2014-12-08 11:37:49 -0800

    user route

---
+++ routes/users_routes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var User = require('..models/user_model.js');
+
+module.exports = function(app, passport) {
+  app.get('/api/users', passport.authenticate('basic',{session: false}, function(req,res){
+    res.json({jwt: req.user.generateToken(app.get('jwtSecret'))});
+  });
+
+  app.post('/api/users', function(req, res) { 
+    User.findOne({'basic.email': req.body.email}, function(err, user){
+      if(err) return res.status(500).send('server error');
+      if(user) return res.status(500).send('cannot create that user');
+
+      var regSpecial = /[a-z A-Z 0-9]{8,32}$/;
+      if(!regSpecial.test(req.body.password)) {
+        return res.status(500).send('bad password');
+      }
+
+      var newUser = new User();
+      newUser.basic.email = req.body.email;
+      newUser.basic.password = newUser.generateHash(req.body.password);
+      newUser.save(function(err){
+        if(err) return res.status(500).send('server error');
+        res.json({jwt: newUser.generateToken(app.get('jwtSecret'))});
+      });
+    });
+  });
+};
