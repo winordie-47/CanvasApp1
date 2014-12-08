759dd0a9bd6438e925e9a3cd709eda4b819fdfc0
Joe Elsey <joe.elsey@gmail.com>
2014-12-07 17:02:58 -0800

    gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function(grunt){
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks("grunt-jscs");
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      src: ['models/**/*.js', 'routes/**/*.js' 'test/**/*.js', 'server.js', 'Gruntfile.js' ]
+    },
+
+    jscs: {
+      src: ['models/**/*.js', 'routes/**/*.js' 'test/**/*.js', 'server.js', 'Gruntfile.js'],
+      options: {
+        config: 'jscsrc'
+      }
+    },
+
+    simplemocha: {
+      src: ['test/api_test.js']
+    }
+  });
+  grunt.registerTask('test', ['jshint','jscs', 'simplemocha']);
+  grunt.registerTask('default',['test']);
+}
