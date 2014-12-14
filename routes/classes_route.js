58e1ef910118cb72a883066f0b4acd546fb357e4
Joe Elsey <joe.elsey@gmail.com>
2014-12-13 17:07:38 -0800

    creating a class based system

--- routes/classes_route.js
+++ routes/classes_route.js
@@ -1,1 +1,29 @@
 'use strict';
+
+module.exports = function(app, jwtauth){
+
+  //creates a class
+  app.post('/api/classes', jwtauth, function(res, res) {
+
+  });
+
+  //gets all of the classes
+  app.get('/api/classes', jwtauth, function(req, res) {
+
+  });
+
+  //gets just one class
+  app.get('/api/class', jwtauth, function(req, res) {
+
+  });
+
+  //changes class info
+  app.put('/api/classes', jwtauth, function(req, res) {
+
+  });
+
+  //deletes a class
+  app.delete('/api/classes', jwtauth, function(req, res) {
+
+  });
+}
